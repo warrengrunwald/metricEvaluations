b4ecdda69f2a412e78768da43c12dca9226584a0
Warren Grunwald <warrengr@mit.edu>
2018-05-10 17:47:28 -0400

    Initial commit

---
+++ plots.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 17:44:08 2018
+
+@author: wcgru
+"""
+
+# Time vs. Entropy
+
+# Time vs. % Map Coverage (normalize based on final % map coverage?)
+
+# Distance vs. Entropy
+
+# Distance vs. % Map Coverage
